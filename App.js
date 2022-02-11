51b933afc5674297f31f25c3261caa1add8e635e
unknown <minaraouf1997@gmail.com>
2022-02-11 21:36:01 +0200

    first commit with home page

---
+++ App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+// import LoginUser from './components/login';
+// import RegisterUser from './components/register';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
+
+import Home from './pages/Home';
+import NavBar from './components/Navbar' ;
+//import Details from './pages/details';
+// import FavFilms from './pages/favouriteFilm';
+// import { LanguageContext } from './context/changeLanguage';
+// import { useState } from 'react';
+
+// import AllMovies from './pages/allMovies'
+
+
+function App() {
+  // console.log(LanguageContext)
+
+  // // const [contextLang,setContextLang] = useState("en")
+  return (
+
+   
+    <Router>
+      
+      <NavBar/> 
+
+      <Switch>
+        <Route path={"/"} exact component={Home}></Route>
+
+      </Switch>
+
+     
+
+    </Router>
+ 
+
+
+  );
+}
+
+export default App;
