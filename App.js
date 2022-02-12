10607992fbd6d0aa11d3372d6d6a176e8abffdf2
Khaled El-Masry <16371159+Khaled-elmasry@users.noreply.github.com>
2022-02-12 13:15:40 +0200

    Propery page

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 // import LoginUser from './components/login';
 // import RegisterUser from './components/register';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
+import './index.css';
+
+import React from 'react';
+import PhotosSlider from './Components/PhotosSlider'
+import PropertyInfo from './Components/PropertyInfo'
+import Description from './Components/Description'
+import Maps from './Components/Maps'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from './pages/Home';
-import NavBar from './components/Navbar' ;
+import NavBar from './components/Navbar';
+
 //import Details from './pages/details';
 // import FavFilms from './pages/favouriteFilm';
 // import { LanguageContext } from './context/changeLanguage';
@@ -14,29 +23,32 @@ import NavBar from './components/Navbar' ;
 // import AllMovies from './pages/allMovies'
 
 
-function App() {
+export default function App() {
   // console.log(LanguageContext)
 
   // // const [contextLang,setContextLang] = useState("en")
   return (
 
-   
+
     <Router>
-      
-      <NavBar/> 
+
+      <NavBar />
+      <PhotosSlider />
+      <PropertyInfo />
+      <Description />
+      {/* <Maps /> */}
 
       <Switch>
         <Route path={"/"} exact component={Home}></Route>
 
       </Switch>
 
-     
+
 
     </Router>
- 
+
 
 
   );
 }
 
-export default App;
